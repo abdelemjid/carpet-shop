a014bd3d7b883f51af0052e7d3a789e7749cb792
root <root@DESKTOP-78PNBNE.localdomain>
2025-09-25 10:47:58 +0100

    Page of Adding new product [for admin].

--- admin/src/apiClient.ts
+++ admin/src/apiClient.ts
@@ -1,3 +1,5 @@
+import type { Product } from "./types/product.type";
+
 const baseUrl = "http://localhost:5000";
 
 export const verifyToken = async () => {
@@ -20,10 +22,25 @@ export const login = async (value: { email: string; password: string }) => {
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify(value),
   });
 
   if (!response.ok) return null;
 
   return await response.json();
+};
+
+export const newProduct = async (productData: FormData) => {
+  const response = await fetch(`${baseUrl}/api/admin/products/new`, {
+    method: "POST",
+    credentials: "include",
+    body: productData,
+  });
+
+  const result = await response.json();
+
+  if (!response.ok) throw new Error(result["error"]);
+
+  return result;
 };
